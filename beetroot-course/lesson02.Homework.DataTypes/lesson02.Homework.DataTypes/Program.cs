078f4d7d22fa088e2c2fa92d31086e9ad47d1197
mandarin <nikita.rudakov.015@gmail.com>
2022-05-02 12:17:53 +0300

    Homework

---
+++ beetroot-course/lesson02.Homework.DataTypes/lesson02.Homework.DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson02.Homework.DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
