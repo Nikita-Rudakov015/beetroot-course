8c52ca47278503575a1223306c073f379d0aafde
mandarin <nikita.rudakov.015@gmail.com>
2022-05-17 15:28:53 +0300

    classwork

---
+++ Lesson09.Enums/Lesson09.Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson09.Enums
+{
+    enum DayOfWeek
+    {
+        Sunday = -15,
+        Monday,
+        Tuesday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek dayOfWeek = DayOfWeek.Monday;
+            foreach (var item in Enum.GetValues<DayOfWeek>())
+            {
+                Console.WriteLine((int)item);
+            }
+
+            DayOfWeek newDayOfWeek = (DayOfWeek)2;
+            Console.WriteLine($"Today is {(DayOfWeek)2}");
+            Console.WriteLine($"Today{dayOfWeek} is {GetHoliday(dayOfWeek)}");
+        }
+
+        private static string GetHoliday(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                    return "non-holiday";
+                case DayOfWeek.Sunday:
+                    return "holiday";
+            }
+            return string.Empty;
+        }
+    }
+}
