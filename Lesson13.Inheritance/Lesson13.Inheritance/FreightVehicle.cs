7f67dba8a091c72a69f3e0000294d3e3f875723c
mandarin <nikita.rudakov.015@gmail.com>
2022-06-29 15:17:52 +0300

    Homework-lesson13-inheritance

---
+++ Lesson13.Inheritance/Lesson13.Inheritance/FreightVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13.Inheritance
+{
+    public class FreightVehicle : Vehicle
+    {
+        public override void AssemblyOfVehicle()
+        {
+            Console.WriteLine("Assembly of freight vehicle!");
+        }
+    }
+}
---
+++ Lesson13.Inheritance/Lesson13.Inheritance/PassengerVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13.Inheritance
+{
+    public class PassengerVehicle : Vehicle
+    {
+        public override void AssemblyOfVehicle()
+        {
+            Console.WriteLine("Assembly of passenger vehicle!");
+        }
+    }
+}
