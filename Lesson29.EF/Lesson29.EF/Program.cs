07cb95d09290314ded69e433601fb54d3997c96b
mandarin <nikita.rudakov.015@gmail.com>
2022-07-09 12:14:21 +0300

    classwork-29-EF is not ready

---
+++ Lesson29.EF/Lesson29.EF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson29.EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
