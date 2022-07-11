e84e17d5aa1cb6c1b23cd4b155cc7bcd473c9b0c
mandarin <nikita.rudakov.015@gmail.com>
2022-07-11 23:19:24 +0300

    classwork lesson29-EF is ready

---
+++ Lesson29.EF/Lesson29.EF/DataAccess/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson29.EF.DataAccess.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Title { get; set; }
+
+        public float Price { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Lesson29.EF/Lesson29.EF/DataAccess/Mappings/CategoryMapping.cs
@@ -0,0 +1,15 @@
+﻿using Lesson29.EF.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Lesson29.EF.DataAccess.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category", "dbo");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
