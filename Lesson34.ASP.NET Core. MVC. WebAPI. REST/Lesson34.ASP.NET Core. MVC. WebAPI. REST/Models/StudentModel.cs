9f11fe88bb22b050433e6e460f57c0a08825684e
mandarin <nikita.rudakov.015@gmail.com>
2022-07-27 21:16:51 +0300

    lesson34-ASP.NET

---
+++ Lesson34.ASP.NET Core. MVC. WebAPI. REST/Lesson34.ASP.NET Core. MVC. WebAPI. REST/Models/StudentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lesson34.ASP.NET_Core._MVC._WebAPI._REST.Models
+{
+    public class StudentModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Lesson34.ASP.NET Core. MVC. WebAPI. REST/Lesson34.ASP.NET Core. MVC. WebAPI. REST/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson34.ASP.NET_Core._MVC._WebAPI._REST.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
