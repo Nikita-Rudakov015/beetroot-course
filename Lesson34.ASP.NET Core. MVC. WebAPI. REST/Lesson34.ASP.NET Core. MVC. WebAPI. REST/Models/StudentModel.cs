065d0f9ada8657af2c92421a893c00f6f42e7d8a
mandarin <nikita.rudakov.015@gmail.com>
2022-07-30 10:04:11 +0300

    .

--- Lesson34.ASP.NET Core. MVC. WebAPI. REST/Lesson34.ASP.NET Core. MVC. WebAPI. REST/Models/StudentModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lesson34.ASP.NET_Core._MVC._WebAPI._REST.Models
-{
-    public class StudentModel
-    {
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-    }
-}
